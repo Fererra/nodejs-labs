79cf6fe015da8ddd0913c84e7de91b8a412aa2c4
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-04-22 21:16:53 +0300

    feat(lab3): initial project setup with MVC layers

---
+++ Lab3/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import indexRouter from "./src/routes/index.js";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
