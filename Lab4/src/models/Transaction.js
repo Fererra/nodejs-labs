4d060597b2962f24aeedab5a49def4a5d6fb969f
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-05-10 15:45:24 +0300

    feat(models): add Category, OperationDate, and TransactionType classes

--- Lab4/src/models/Transaction.js
+++ Lab4/src/models/Transaction.js
@@ -1,23 +1,13 @@
-/**
- * Клас для опису фінансової транзакції (витрати чи надходження)
- */
 class Transaction {
-  /**
-   * @param {number} id - Унікальний ідентифікатор
-   * @param {'income' | 'expense'} type - Тип: надходження або витрата
-   * @param {number} amount - Сума
-   * @param {string} purchase - Назва покупки / опис надходження
-   * @param {string} category - Категорія
-   * @param {string} date - Дата у форматі YYYY-MM-DD
-   */
-  constructor(id, type, amount, purchase, category, date) {
+  constructor(id, amount, description, typeId, categoryId, dateId) {
     this.id = id;
-    this.type = type;
     this.amount = amount;
-    this.purchase = purchase;
-    this.category = category;
-    this.date = date;
+    this.description = description;
+
+    this.typeId = typeId;
+    this.categoryId = categoryId;
+    this.dateId = dateId;
   }
 }
 
-export default Transaction;
+export { Category, OperationDate, TransactionType, Transaction };
