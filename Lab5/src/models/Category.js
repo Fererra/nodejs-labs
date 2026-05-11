effbfa42dd6b25e144fad842042acd4ea598eed7
M1RVKL <bielov.vlad@gmail.com>
2026-05-11 10:54:27 +0300

    Category.js and TransactionType.js updated

--- Lab5/src/models/Category.js
+++ Lab5/src/models/Category.js
@@ -1,6 +1,27 @@
-class Category {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../../config/database.js';
+
+class Category extends Model {}
+
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true
+    }
   }
-}
+}, {
+  sequelize,
+  modelName: 'Category',
+  tableName: 'categories',
+  timestamps: false
+});
+
+export default Category;
--- Lab5/src/models/TransactionType.js
+++ Lab5/src/models/TransactionType.js
@@ -1,6 +1,27 @@
-class TransactionType {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../../config/database.js';
+
+class TransactionType extends Model {}
+
+TransactionType.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isIn: [['income', 'expense']]
+    }
   }
-}
+}, {
+  sequelize,
+  modelName: 'TransactionType',
+  tableName: 'transaction_types',
+  timestamps: false
+});
+
+export default TransactionType;
